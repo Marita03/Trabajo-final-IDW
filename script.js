f57fdea28c018368b47063ca4dc2bcf91be28e95
María Ignacia <Miorellanam@uc.cl>
2025-11-27 11:42:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-const portfolio = document.querySelector("#popote");
-
-fetch("datos.json")
+fetch("proyectos.json")
   .then(response => response.json())
-  .then(trabajos => {
+  .then(data => {
 
-    trabajos.forEach((trabajo) => {
+    const galeria = document.getElementById("galeria");
 
-      portfolio.innerHTML += `
-      <div class="col">
-        <div class="card shadow-sm h-100">
-          <img src="${trabajo.photo}" class="card-img-top">
-          <div class="card-body">
-            <h5>${trabajo.title}</h5>
-            <p class="categoria">${trabajo.category}</p>
-            <p>${trabajo.description}</p>
-          </div>
-        </div>
-      </div>
-      `;
-
-    });
-
-  })
-  .catch(error => {
-    console.error("Error cargando los proyectos:", error);
+    if (galeria) {
+      data.forEach(p => {
+        galeria.innerHTML += `
+          <div class="tarjeta">
+            <img src="${p.photo}">
+            <h3>${p.title}</h3>
+            <p>${p.category}</p>
+            <p>${p.description}</p>
+          </div>`;
+      });
+    }
   });
+
+// habilidades
+const habilidades = [
+  { programa: "Illustrator", valor: 90 },
+  { programa: "Fusion 360", valor: 70 },
+  { programa: "Twinmotion", valor: 40 },
+  { programa: "Photoshop", valor: 30 },
+  { programa: "Inglés", valor: 70 },
+];
+
+const tabla = document.getElementById("aqui");
+
+if (tabla) {
+  habilidades.forEach(h => {
+    tabla.innerHTML += `
+      <tr>
+        <td>${h.programa}</td>
+        <td>
+          <svg viewBox="0 0 100 6" width="200">
+            <rect width="100" height="4" fill="#ded4cd" rx="2"/>
+            <rect width="${h.valor}" height="4" fill="darkseagreen" rx="2"/>
+          </svg>
+        </td>
+      </tr>`;
+  });
+}
