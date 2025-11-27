eef6debcc861e81416c4cdd0c6ba6da5959344aa
María Ignacia <Miorellanam@uc.cl>
2025-11-27 14:49:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const portfolio = document.querySelector("#popote");
+const contenedor = document.querySelector("#popote");
+
 
 async function cargarProyectos(url) {
   try {
     const respuesta = await fetch(url);
-    const datos = await respuesta.json();
-
-    const proyectos = datos.data;
+    const proyectos = await respuesta.json(); 
 
     proyectos.forEach((proyecto) => {
-      portfolio.innerHTML += `
+      contenedor.innerHTML += `
         <div class="proyecto-card">
-          <img src="${proyecto.photo}" alt="${proyecto.title}">
-          
+          <img src="${proyecto.photo}" alt="${proyecto.title}" />
+
           <div class="proyecto-info">
             <h3>${proyecto.title}</h3>
             <p>${proyecto.description}</p>
-            <span class="categoria">${proyecto.category}</span>
+            <span>${proyecto.category}</span>
           </div>
         </div>
       `;
     });
 
   } catch (error) {
-    console.error("Error cargando proyectos:", error);
+    console.error("Error al cargar proyectos:", error);
   }
 }
 
-cargarProyectos("https://api.myjson.online/v1/records/72a0a513-be36-4a6c-b360-812151ac4e75");
+cargarProyectos("RUTA_DE_TU_JSON.json");
