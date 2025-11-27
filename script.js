f7c09878aa4a1d6adf22f8fe849efe30bb8da1ab
María Ignacia <Miorellanam@uc.cl>
2025-11-27 19:05:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Solo busca el contenedor si existe
+
+// HABILIDADES
+const habilidades = [
+  { programa: "Illustrator", valor: 90 },
+  { programa: "Fusion 360", valor: 70 },
+  { programa: "Twinmotion", valor: 40 },
+  { programa: "Photoshop", valor: 30 },
+  { programa: "Inglés", valor: 70 },
+];
+
+const contenedorHabilidades = document.querySelector("#contenedor-habilidades");
+
+if (contenedorHabilidades) {
+  habilidades.forEach((h) => {
+    contenedorHabilidades.innerHTML += `
+      <div class="habilidad-item">
+        <span>${h.programa}</span>
+        <div class="barra">
+          <div style="width:${h.valor}%"></div>
+        </div>
+      </div>
+    `;
+  });
+}
+
+
 const contenedor = document.querySelector("#contenedor-proyectos");
 
 async function cargarProyectos(url) {
-  if (!contenedor) return; // Si no existe, no hace nada
+  if (!contenedor) return; 
 
   try {
     const respuesta = await fetch(url);
-    const proyectos = await respuesta.json(); 
+    const proyectos = await respuesta.json();
 
-    contenedor.innerHTML = ""; // Limpia antes de cargar
+    contenedor.innerHTML = ""; 
 
     proyectos.forEach((proyecto) => {
       contenedor.innerHTML += `
@@ -28,24 +53,28 @@ async function cargarProyectos(url) {
   }
 }
 
-// AJUSTA ESTA RUTA si tu JSON está en otra carpeta
+// Ajusta la ruta si es necesario
 cargarProyectos("./proyectos.json");
 
 
+
 // BOTÓN SUBIR
+
 const btnSubir = document.getElementById("btnSubir");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    btnSubir.style.display = "block";
-  } else {
-    btnSubir.style.display = "none";
-  }
-});
-
-btnSubir.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
+if (btnSubir) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      btnSubir.style.display = "block";
+    } else {
+      btnSubir.style.display = "none";
+    }
   });
-});
+
+  btnSubir.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
